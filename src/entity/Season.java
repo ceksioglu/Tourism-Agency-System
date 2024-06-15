db351af6d7f6c3a39d1b892a576aabb934b255a3
ceksioglu <64319097+ceksioglu@users.noreply.github.com>
2024-06-15 17:19:13 +0300

    Create Season.java

---
+++ src/entity/Season.java
@@ -0,0 +1,50 @@
+package entity;
+
+import java.sql.Date;
+
+public class Season {
+    private int id;
+    private int hotelId;
+    private Date startDate;
+    private Date endDate;
+
+    public Season(int id, int hotelId, Date startDate, Date endDate) {
+        this.id = id;
+        this.hotelId = hotelId;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    // Getters and setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getHotelId() {
+        return hotelId;
+    }
+
+    public void setHotelId(int hotelId) {
+        this.hotelId = hotelId;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+}
